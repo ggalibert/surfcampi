0c774a31c1a9338e03e778a1c7cef6e61a86023e
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-10 18:11:22 +1000

    First version

--- surfcampi.sh
+++ surfcampi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./collectPictures.sh
 animatedFile=`./createAnimatedGif.sh`
-lftp -e "put -O /surfcampi/ $animatedFile; bye" -u parliament,phtj4pdwj ftp.users.on.net
+lftp -e "put -O /surfcampi/ $animatedFile; bye" -u $USER,$PASS ftp.users.on.net
 exit 0
