5310b5edc5efce17818f94ff5a3f430940fcc986
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-11 16:46:33 +1000

    back to testing with no halt and alarm every minute

--- startSurfcampi.sh
+++ startSurfcampi.sh
@@ -2,5 +2,4 @@
 cd /home/pi/surfcampi/
 git pull &> startSurfcampi.log
 ./surfcampi.sh &>> startSurfcampi.log 
-sudo halt  &>> startSurfcampi.log
 exit 0
--- surfcampi.sh
+++ surfcampi.sh
@@ -2,4 +2,5 @@
 ./collectPictures.sh
 animatedFile=`./createAnimatedGif.sh`
 lftp -e "put -O /surfcampi/ $animatedFile; bye" -u $USER,$PASS ftp.users.on.net
+#sudo halt
 exit 0
