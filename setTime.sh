76450271219dfb30e404dbc3bfdfd21e702de404
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-15 08:48:43 +1000

    Fix modprob

--- setTime.sh
+++ setTime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "ds1374 0x68" > /sys/class/i2c-adapter/i2c-1/new_device
+/bin/bash "echo ds1374 0x68 > /sys/class/i2c-adapter/i2c-1/new_device"
  
 # Check for an IP address
 _IP=$(hostname -I) || true
