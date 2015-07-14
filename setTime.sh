f7ddccf24c79fa371dc5a19a815fc1b047839f0a
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-15 08:39:01 +1000

    fix first line in bash

--- setTime.sh
+++ setTime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/bash -c "echo ds1374 0x68 > /sys/class/i2c-adapter/i2c-1/new_device"
+echo ds1374 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
  
 # Check for an IP address
 _IP=$(hostname -I) || true
