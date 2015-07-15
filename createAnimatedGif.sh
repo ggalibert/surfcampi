600b088e59ce5d7d0825f4e31f13298496d0bd88
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-15 18:11:01 +1000

    fixed output file name

--- createAnimatedGif.sh
+++ createAnimatedGif.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 hour=$1
-outputFile=/tmp/surfcampi_$hour.gif
+outputFile="/tmp/surfcampi_$hour.gif"
 convert -delay 30 -loop 0 /tmp/surfcampi_*.jpg $outputFile
 echo $outputFile
 exit 0
