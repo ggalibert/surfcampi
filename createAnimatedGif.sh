eac924c90e36e32c9e8fa3335160f48349391099
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-15 17:48:25 +1000

    options to convert are declared in vars. hour var is now passed in argument

--- createAnimatedGif.sh
+++ createAnimatedGif.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-hour=`date +%H`
-convert -delay 10 -loop 0 /tmp/surfcampi_*.jpg /tmp/surfcampi_$hour.gif
-echo /tmp/surfcampi_$hour.gif
+hour=$1
+outputFile=/tmp/surfcampi_$hour.gif
+convert -delay 30 -loop 0 /tmp/surfcampi_*.jpg $outputFile
+echo $outputFile
 exit 0
