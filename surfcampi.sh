35dfa980935a3ab9c179e267d6b404ec2643d33b
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-07-15 17:50:38 +1000

    Added comments. Surfcampi is now working only during working hours. Makes use of uploadFTP.sh and updated createAnimatedGif.sh

--- surfcampi.sh
+++ surfcampi.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
+
+# set RTC time from system time if connected to network
+# otherwise set system time from RTC.
 sudo ./setTime.sh
+
+# print current date time
 date
+
+# update scripts from github repo
 git pull
-./collectPictures.sh
-animatedFile=`./createAnimatedGif.sh`
-lftp -e "put -O /surfcampi/ $animatedFile; bye" -u $USER,$PASS ftp.users.on.net
+
+# define hours for when surfcampi is working
+workingHours=(`cat ./workingHours.txt`)
+hour=`date +%H`
+
+# find out whether surfcampi should be working or not
+isWorkingHour=0
+for wHour in "${workingHours[@]}"
+do
+  if [ $wHour = $hour ]; then
+    isWorkingHour=1
+  fi
+done
+
+if [ $isWorkingHour = 1 ]; then
+  # surfcampi is working
+
+  # run script to collect pictures
+  ./collectPictures.sh
+
+  # run script to create an animated gif from the collected pictures
+  animatedFile=`./createAnimatedGif.sh $hour`
+
+  # upload animated gif to FTP
+  ./uploadToFTP.sh $animatedFile
+else
+  # surfcampi does nothing for 3min in case we want to ssh it
+  sleep 180
+fi
+
+# shutdown surfcampi
 sudo halt
+
 exit 0
